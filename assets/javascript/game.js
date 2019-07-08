b852f963be7f05be2fe8059e05af5f1404be755e
Mark Jones <mxj492@gmail.com>
2019-07-08 19:44:00 -0400

    updating changes to files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,16 @@
-//globle variables
+
 var wins = 0;
 var losses = 0;
-//players currunt score
+
 var addition = 0;
-//selects a random number from 19 t0 120
+
 var total = Math.floor((Math.random()* 100) + 19);
-//assigns random value to crystals from 1-12
+
 var red = Math.floor((Math.random()* 12) + 1);
 var blue = Math.floor((Math.random()* 12) + 1);
 var yellow = Math.floor((Math.random()* 12) + 1);
 var green = Math.floor((Math.random()* 12) + 1);
-var audio = new Audio('https://files.slack.com/files-pri/T0J8XK2Q3-F0NHG3FDZ/download/raven.mp3');
 
-//updates addition in html
 var updateAddition = function (){
 	$('.addition').empty();
 	$('.addition').append(addition);
@@ -21,7 +19,7 @@ var updateAddition = function (){
 	$('#losses').empty();
 	$('#losses').append(losses);
 }
-//restart function
+
 var restart = function(){
 	addition = 0;
     total = Math.floor((Math.random()* 100) + 19);
@@ -33,19 +31,19 @@ var restart = function(){
     blue = Math.floor((Math.random()* 12) + 1);
     yellow = Math.floor((Math.random()* 12) + 1);
     green = Math.floor((Math.random()* 12) + 1);
-    //alert('restart');
+   
     updateAddition();
 }
 var logic = function (){
 	if (addition == total) {
 		wins = wins + 1;
 		
-		audio.play();
+		
 		restart();
 	}else if(addition > total){
 		losses = losses + 1;
 		
-		audio.play();
+		
 		restart();
 	}else{
 		updateAddition();
@@ -54,7 +52,7 @@ var logic = function (){
 $('.total').append(total);
 
 $('.addition').append(addition);
-//function
+
 
 $(document).ready(function(){
 	$('#red').click(function(){
